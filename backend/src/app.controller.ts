e29f165a3efcfd20372fc734eb50082ccd0c1cb4
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2026-02-05 15:52:34 +0100

    first commit

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UsersService } from './users/user.service';
+import type { UserModel } from 'generated/prisma/models';
+import { CreateUserDto } from './users/CreateUserDto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('user')
+  async signupUser(@Body() userData: CreateUserDto): Promise<UserModel> {
+    return this.usersService.createUser(userData);
+  }
+
+  @Get('users')
+  async getAllUsers(): Promise<UserModel[]> {
+    return this.usersService.users({});
   }
 }
