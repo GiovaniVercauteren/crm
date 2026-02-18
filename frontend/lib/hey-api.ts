a1dab30d40d25246b0090d3f0848df5bbcd44583
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2026-02-18 12:57:50 +0100

    feat: get backend url from environment

--- frontend/lib/hey-api.ts
+++ frontend/lib/hey-api.ts
@@ -2,5 +2,5 @@ import { type CreateClientConfig } from "./client/client.gen";
 
 export const createClientConfig: CreateClientConfig = (config) => ({
   ...config,
-  baseUrl: "http://127.0.0.1:4000", // Update this to your backend URL
+  baseUrl: process.env.BACKEND_URL, // Update this to your backend URL
 });
