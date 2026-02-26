17ef1eb76341e65fe8effc746be51e10e5ed56e5
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2026-02-26 17:20:02 +0100

    fix entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -69,4 +69,4 @@ npx drizzle-kit migrate
 
 echo "Starting NestJS application"
 # 'exec' makes the node process PID 1, which is better for handling Linux signals (like shutdown)
-exec node dist/src/main.js
+exec node dist/main.js
