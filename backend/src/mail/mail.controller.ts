e2b8b0801cb779c48d364c56e58ef1d13242ad26
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2026-02-26 18:23:45 +0100

    protect mail endpoints

--- backend/src/mail/mail.controller.ts
+++ backend/src/mail/mail.controller.ts
@@ -1,21 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { SendMailDto } from './dto/send-mail.dto';
-import { Public } from 'src/decorators/public.decorator';
 import { SendTemplatedMailDto } from './dto/send-templated-mail.dto';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
-  @Public()
   @Post('send')
   async sendMail(@Body() sendMailDto: SendMailDto): Promise<void> {
     const { to, subject, text, html } = sendMailDto;
     await this.mailService.sendMail(to, subject, text, html);
   }
 
-  @Public()
   @Post('send-templated')
   async sendTemplatedMail(
     @Body() sendTemplatedMailDto: SendTemplatedMailDto,
